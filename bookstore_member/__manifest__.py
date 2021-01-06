182dc5123a5707a713fbc5d0708a41a6988a7e59
kherney <kevinh939@gmail.com>
2021-01-05 21:56:58 -0500

    [Add] field to book model. Inherit view.

--- bookstore_member/__manifest__.py
+++ bookstore_member/__manifest__.py
@@ -5,7 +5,9 @@
     'description': 'Manage people who will be able to borrow books.',
     'author': 'Kevin Herney R.',
     'depends': ['bookstore'],
-    'data': [],
+    'data': [
+        "views/bookstore_member_book.xml"
+    ],
     'installable': True,
     'auto_install': False,
     'applications': False,
--- bookstore_member/models/bookstore_member_book.py
+++ bookstore_member/models/bookstore_member_book.py
@@ -5,3 +5,5 @@ class BookstoreMember(models.Model):
     _inherit = "bookstore.book"
 
     is_available = fields.Boolean("Is available")
+    book_isbn = fields.Char(help="An ISBN is an International Standard Book Number. ISBNs were 10 digits")
+    book_publisher = fields.Many2one(index=True)
