c57cf7cd6265b4fa71b44b03528a2286422beebd
kherney <kevinh939@gmail.com>
2021-01-09 22:31:15 -0500

    [Add] Reuse/inherit _check_isbn function from bookstore.book

--- bookstore_member/models/bookstore_member_book.py
+++ bookstore_member/models/bookstore_member_book.py
@@ -7,3 +7,14 @@ class BookstoreMember(models.Model):
     is_available = fields.Boolean("Is available")
     book_isbn = fields.Char(help="An ISBN is an International Standard Book Number. ISBNs were 10 digits")
     book_publisher = fields.Many2one(index=True)
+
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.book_isbn if x.isdigit()]
+        if len(digits) == 10:
+            ponderators = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            total = sum(a * b for a, b in zip(digits[:9], ponderators))
+            check = total % 11
+            return digits[-1] == check
+        else:
+            return super()._check_isbn()
