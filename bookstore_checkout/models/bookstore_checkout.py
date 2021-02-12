8f69887116427ef196a7181544157da4bb159d86
kherney <kevinh939@gmail.com>
2021-02-12 10:38:20 -0500

    [FIX] a bad named atrr in Many2one field. comodel_name

--- bookstore_checkout/models/bookstore_checkout.py
+++ bookstore_checkout/models/bookstore_checkout.py
@@ -5,10 +5,13 @@ class BookstoreCheckout(models.Model):
     _name = "bookstore.checkout"
     _description = "Checkout Request"
 
-    member_id = fields.Many2one(comodel="bookstore.member", required=True)
-    user_id = fields.Many2one(comodel="res.user",
-                              name="Librarìan",
-                              default=lambda s: fields.env.uid)
+    user_id = fields.Many2one(comodel_name="res.users",
+                              name="Librarian",
+                              default=2,
+                              readonly=True)
+    member_book_id = fields.Many2one("bookstore.member",
+                                     string="Librarian",
+                                     required=True)
     request_date = fields.Date(
         default=lambda s: fields.Date.today())
 
@@ -20,6 +23,6 @@ class BookstoreCheckout(models.Model):
 class BookstoreCheckout(models.Model):
     _name = "bookstore.checkout.line"
     _description = "Checkout Request lines"
-    checkout_id = fields.Many2one(comodel="bookstore.checkout")
-    book_id = fields.Many2one(comodel="bookstore.book")
+    checkout_id = fields.Many2one(comodel_name="bookstore.checkout")
+    book_id = fields.Many2one(comodel_name="bookstore.book")
 
