d5345f677b07c758f1675b9bdf28b6e842a9bf13
kherney <kevinh939@gmail.com>
2021-03-01 10:37:10 -0500

    [FIX] default function in stage_id

--- bookstore_checkout/models/bookstore_checkout.py
+++ bookstore_checkout/models/bookstore_checkout.py
@@ -6,7 +6,7 @@ class BookstoreCheckout(models.Model):
     _description = "Checkout Request"
 
     user_id = fields.Many2one(comodel_name="res.users",
-                              name="Librarian",
+                              name="User",
                               default=2,
                               readonly=True)
     member_book_id = fields.Many2one("bookstore.member",
@@ -21,8 +21,9 @@ class BookstoreCheckout(models.Model):
 
     stage_id = fields.Many2one(comodel_name="bookstore.checkout.stage",
                                string="States",
-                               group_expand="_group_expand_stage_id",
-                               default="_default_stage",)
+                               default=lambda self: self._default_stage,
+                               group_expand="_group_expand_stage_id")
+    state = fields.Selection(related="stage_id.state")
 
     @api.model
     def _default_stage(self):
@@ -39,4 +40,3 @@ class BookstoreCheckout(models.Model):
     _description = "Checkout Request lines"
     checkout_id = fields.Many2one(comodel_name="bookstore.checkout")
     book_id = fields.Many2one(comodel_name="bookstore.book")
-
